44bae0b55613ae3692b382d2e0d274198623f730
Paraschiv Andrei-Dumitru <pandreyy2014@gmail.com>
2020-04-26 22:39:33 +0300

    Add High Score
    - High Score stored to file
    - Table view of last 5 high scores

---
+++ Assets/DataFiles/Scripts/DataManager.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class DataManager
+{
+    private static DataManager _instance;
+    public static DataManager Instance { get { return _instance; } }
+    
+    private string filename = "highs-scores.txt";
+    public Player player;
+
+    public DataManager()
+    {
+        string path = GetFilePath(filename);
+        if (File.Exists(path))
+        {
+            Load();
+        }
+        else
+        {
+            this.player = new Player();
+        }
+        
+    }
+
+    public int GetScore(int pos)
+    {
+        return player.getScore(pos);
+    }
+    
+    public void InsertScore(int score)
+    {
+        this.player.insertScore(score);
+    }
+
+    public string GetPlayerName()
+    {
+        return this.player.Name;
+    } 
+    
+    public int GetCapacity()
+    {
+        return this.player.getCapacity();
+    }
+
+    public void Save()
+    {
+        string json = JsonUtility.ToJson(player);
+        WriteToFile(filename, json);
+    }
+
+    public void Load()
+    {
+        player = new Player();
+        string json = ReadFromFile(filename);
+        JsonUtility.FromJsonOverwrite(json, player);
+    }
+
+    private void WriteToFile(string filename, string json)
+    {
+        string path = GetFilePath(filename);
+        FileStream fileStream = new FileStream(path, FileMode.Create);
+
+        using (StreamWriter writer = new StreamWriter(fileStream))
+        {
+            writer.Write(json);
+        }
+    }
+
+    private string ReadFromFile(string filename)
+    {
+        string path = GetFilePath(filename);
+
+        if (File.Exists(path))
+        {
+            using (StreamReader reader = new StreamReader(path))
+            {
+                string json = reader.ReadToEnd();
+
+                return json;
+            }
+        } else 
+            Debug.LogWarning("Fatal error! File not found!");
+
+        return "";
+
+    }
+    
+    private string GetFilePath(string filename)
+    {
+        return Application.persistentDataPath + '/' + filename;
+    }
+}
+
+public class Player
+{
+    private string playerName;
+    public List<int> highscores;
+
+    public Player()
+    {
+        this.playerName = "Current Player";
+        this.highscores = new List<int>();
+    }
+    
+    public string Name { get { return playerName; } set { playerName = value; } }
+
+    public int getScore(int pos)
+    {
+        return this.highscores[pos];
+    }
+    
+    public void insertScore(int score)
+    {
+        this.highscores.Sort((a, b) => b.CompareTo(a));
+        if (getCapacity() >= 5)
+        {
+            this.highscores[getCapacity() - 1] = score;
+            this.highscores.Sort((a, b) => b.CompareTo(a));
+            this.highscores = this.highscores.GetRange(0, 5);
+        }
+        else
+        {
+            this.highscores.Add(score);    
+        }
+    }
+
+    public int getCapacity()
+    {
+        return this.highscores.Count;
+    }
+}
--- Assets/DataFiles/Scripts/PlayerScore.cs
+++ Assets/DataFiles/Scripts/PlayerScore.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -12,16 +14,31 @@ public class PlayerScore : MonoBehaviour
     public int currentPoints { get; set; }
     public int highScore { get; set; }
     public Text points;
+    
+    public string file = "highs-scores.txt";
+    private string message;
+    public DataManager dataManager;
+
+    public override string ToString()
+    {
+        return $"{base.ToString()}";
+    }
 
     private void UpdatePoints()
     {
         points.text = currentPoints.ToString() + " points";
     }
 
+    private void Start()
+    {
+        dataManager = new DataManager();
+    }
+
     private void Awake()
     {
         if (_instance != null && _instance != this)
         {
+            dataManager.Save();
             Destroy(this.gameObject);
         } else
         {
@@ -29,6 +46,8 @@ public class PlayerScore : MonoBehaviour
             _instance.currentPoints = 0;
             _instance.highScore = PlayerPrefs.GetInt("HighScore", 0);
             _instance.points.text = "0 points";
+            _instance.message = "0 points";
+            _instance.dataManager = new DataManager();
         }
     }
 
@@ -56,5 +75,18 @@ public class PlayerScore : MonoBehaviour
         {
             PlayerPrefs.SetString("LastScore", currentPoints.ToString() + " points\n High Score :" + highScore.ToString());
         }
+    }
+
+    void OnDestroy()
+    {
+        dataManager.InsertScore(currentPoints);
+        dataManager.Save();
+    }
+
+    void OnApplicationPause(bool pauseStatus)
+    {
+        // for mobile devices
+        if (pauseStatus)
+            dataManager.Save();
     }
 }
